7f472b42d4d29480474e62f63d6db18a39d57e94
nils <nils@koyeb.com>
2022-05-10 21:20:32 +0200

    Initial commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "@remix-run/react";
+import Item from "~/components/Item";
+import { getItem, getTopStories } from "~/helpers/fetch";
+
+export const loader = async () => {
+  const topStoryIds = await getTopStories();
+  const items = await Promise.all(
+    topStoryIds.slice(0, 10).map(async (itemId) => await getItem(itemId))
+  );
+
+  return items;
+};
+
+export default function Index() {
+  const items = useLoaderData();
+
+  return (
+    <div className="divide-y">
+      {items.length > 0 &&
+        items.map((item) => {
+          return <Item item={item} key={item.id} />;
+        })}
+    </div>
+  );
+}
